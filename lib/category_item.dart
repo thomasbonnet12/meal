9d7d9f8be45ca5f412af6bb31db7346cbaf57f46
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-02-05 22:00:49 +0100

    id constructor added to categoryMealsScreen and CategoryItem, the id and title know can move between these 2 object and the categoryScreen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_meal_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  const CategoryItem(this.title, this.color);
+  const CategoryItem(this.title, this.color, this.id);
 
+  //My Navigator function Below
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return CategoryMealsScreen();
+      return CategoryMealsScreen(id, title);
     }));
   }
 
