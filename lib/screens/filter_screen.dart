e53511ae7882eb41fc55e5a031b465bdeb179134
thomasbonnet12 <thomas.bonnet.12@gmail.com>
2021-02-16 21:37:29 +0100

    drawer in the setting page

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/widgets/main_drawer.dart';
 
 class FilterScreen extends StatelessWidget {
   static const routeName = '/filters';
@@ -6,6 +7,10 @@ class FilterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Settings'),
       ),
